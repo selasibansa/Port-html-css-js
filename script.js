3d3dfe1b70806c50b9d9511bb01b1b5955d74702
selasibansa <max.bansa@gmail.com>
2023-10-08 23:26:58 +0000

    made changes

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-link");
+    const icon = document.querySelector(".hamburgur-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
